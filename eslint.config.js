cf22c9400415f739104f6d673da245d58c149d27
Andrey <andreybespaliy69@gmail.com>
2024-06-19 21:43:51 +0300

    Added some rules to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -8,43 +8,53 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-	{
-		ignores: ['dist', 'node_modules', 'coverage', 'eslint.config.js'],
-	},
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-				...globals.es2021,
-			},
-			parserOptions: eslintReact.configs.recommended.parserOptions,
-		},
-	},
-	js.configs.recommended,
-	{
-		plugins: {
-			react: eslintReact,
-			'react-hooks': eslintReactHooks,
-			'react-refresh': eslintReactRefresh,
-			prettier: prettierPlugin,
-		},
-		rules: {
-			...eslintConfigPrettier.rules,
-			'no-console': 'warn',
-			'no-extra-semi': 'error',
-			'no-empty': ['error', { allowEmptyCatch: true }],
-			'no-unreachable': 'error',
-			'prefer-const': 'error',
-			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-			'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
-			'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
-			'react/self-closing-comp': ['error', { component: true, html: true }],
-			'max-lines': ['warn', { max: 124 }],
-			'prettier/prettier': 'error',
-		},
-	},
-	{
-		files: ['**/*.{js,jsx,ts,tsx}'],
-	},
+    {
+        ignores: ['dist', 'node_modules', 'coverage', 'eslint.config.js'],
+    },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                ...globals.es2021,
+            },
+            parserOptions: eslintReact.configs.recommended.parserOptions,
+        },
+    },
+    js.configs.recommended,
+    {
+        plugins: {
+            react: eslintReact,
+            'react-hooks': eslintReactHooks,
+            'react-refresh': eslintReactRefresh,
+            prettier: prettierPlugin,
+        },
+        rules: {
+            ...eslintConfigPrettier.rules,
+            'no-console': 'warn',
+            'no-debugger': 'error',
+            'no-undef': 'error',
+            'no-extra-semi': 'error',
+            'no-empty': ['error', { allowEmptyCatch: true }],
+            'no-unreachable': 'error',
+            'prefer-const': 'error',
+
+            // Code style
+            'semi': ['error', 'always'],
+            'indent': ['error', 4],
+            'quotes': ['error', 'single'],
+            'comma-dangle': ['error', 'always-multiline'],
+
+            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+            'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
+            'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
+            'react/self-closing-comp': ['error', { component: true, html: true }],
+			'max-lines': ['warn', { max: 100 }],
+			
+            // 'prettier/prettier': 'error',
+        },
+    },
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+    },
 ];
