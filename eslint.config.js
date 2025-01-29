4ff3186eda44d42e8847e2a642f4b77dd4a96c79
Andrey <andreybespaliy69@gmail.com>
2025-01-29 12:59:34 +0200

    build: update all dependencies and set up config
    - update: react, react-dom, and other dependencies
    - set up eslint together with typescript and prettier
    - set up typescript config
    - add declarations.d.ts file for style modules

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,67 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import importPlugin from 'eslint-plugin-import';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'eslint.config.js'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      importPlugin.flatConfigs.recommended,
+    ],
+    files: ['**/*.{js,jsx,ts,tsx,}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2025,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+        tsconfigRootDir: __dirname,
+        ecmaFeatures: { jsx: true },
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+          alwaysTryTypes: true,
+        },
+        node: true,
+      },
+    },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      // general options
+      'prettier/prettier': 'warn',
+
+      // imports
+      'import/exports-last': 'warn',
+      'import/imports-first': 'warn',
+      'import/no-cycle': 'error',
+      'import/no-self-import': 'error',
+      'import/newline-after-import': 'warn',
+      'import/order': 'warn',
+      'import/imports-first': 'off', // deprecated
+      'import/first': 'warn', // instead of import/imports-first
     },
   },
-)
+  eslintPluginPrettierRecommended,
+);
