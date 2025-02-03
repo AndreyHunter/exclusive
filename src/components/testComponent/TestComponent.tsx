50331b67dc6fb18127ae6dd61c0112c3a06eca3b
Andrey <andreybespaliy69@gmail.com>
2025-02-03 19:11:39 +0200

    feat(TestCompoent): add initial structure and styles

--- src/components/testComponent/TestComponent.tsx
+++ src/components/testComponent/TestComponent.tsx
@@ -6,6 +6,14 @@ export const TestComponent: React.FC = () => {
   return (
     <div className={styles.root} data-testid="test-component">
       HELLO FROM PULL REQUEST!
+      <article className={styles.card}>
+        <header>
+          <h2>TITLE</h2>
+        </header>
+        <section>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi, eius!</p>
+        </section>
+      </article>
     </div>
   );
 };
