574cd026fc0a39914ce8241e49633e6732c4e1bd
Andrey <148604428+AndreyHunter@users.noreply.github.com>
2025-03-17 14:28:02 +0200

    feat: enhance filtering and navigation experience
    - save applied filters in URL query parameters
    - implement parsing and serializing of query parameters using 'query-string' library
    - create SortSelect component for result ordering
    - add automatic filter reset when changing categories
    - update URL query params synchronization for persistent state

--- src/pages/productsPage/ProductsPage.tsx
+++ src/pages/productsPage/ProductsPage.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { SectionTitle } from '@components/atoms/sectionTitle/SectionTitle';
 import { Container } from '@components/helpers/container/Container';
 import { BreadCrumbs } from '@components/molecules/breadCrumbs/BreadCrumbs';
@@ -5,6 +7,8 @@ import { ProductsList } from '@components/molecules/productList/ProductList';
 import type { Product, BreadCrumbsType } from 'types/index';
 import { Loader } from '@components/atoms/loader/Loader';
 import { FilterPanel } from '@components/organisms/filterPanel/FilterPanel';
+import { Flex } from '@/components/helpers/flex/Flex';
+import { SortSelect } from '@components/molecules/sortSelect/SortSelect';
 
 import styles from './productsPage.module.scss';
 
@@ -23,11 +27,27 @@ const ProductsPage = ({
   breadCrumbs,
   categoryName,
 }: ProductsPageProps) => {
+  const [prevProducts, setPrevProducts] = useState(products);
+
+  useEffect(() => {
+    if (!loading) {
+      setPrevProducts(products);
+    }
+  }, [products, loading]);
+
   return (
     <>
       <Container>
         <BreadCrumbs elements={breadCrumbs} />
         <SectionTitle className={styles.title}>{categoryName}</SectionTitle>
+        <Flex justifyContent="space-between" className={styles.panel}>
+          <span>
+            {loading
+              ? `Found ${prevProducts.length} products`
+              : `Found ${products.length} products`}
+          </span>
+          <SortSelect />
+        </Flex>
         <div className={styles.content}>
           <FilterPanel />
           {loading ? (
--- src/pages/productsPage/ProductsPageContainer.tsx
+++ src/pages/productsPage/ProductsPageContainer.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect, useState, useRef } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
 
 import { useAppDispatch, useAppSelector } from '@/app/hooks';
 import {
@@ -14,7 +15,14 @@ import {
   fetchFlashSales,
   selectFlashSales,
 } from '@features/products/productsSlice';
-import { selectAllFilters } from '@features/productFilters/productFiltersSlice';
+import type { SortBy } from '@features/productFilters/productFiltersSlice';
+import {
+  selectAllFilters,
+  setPriceRange,
+  setSortBy,
+  setSpecificFilter,
+  resetFilters,
+} from '@features/productFilters/productFiltersSlice';
 import { Utils } from '@utils/index';
 
 import ProductsPage from './ProductsPage';
@@ -32,24 +40,74 @@ const ProductPageContainer = () => {
   const [limit, setLimit] = useState(20);
   const [page, setPage] = useState(1);
 
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
   const fullPath = pathname.replace('/products', '');
+  const navigate = useNavigate();
+  const [isFiltersLoaded, setIsFiltersLoaded] = useState(false);
+  const prevPathname = useRef(pathname);
 
   const categoryName = Utils.getCategoryName(fullPath);
   const breadCrumbs = Utils.generateBreadcrumbs(pathname);
 
   useEffect(() => {
-    dispatch(clearProducts());
-    if (pathname === '/products') {
-      dispatch(fetchProducts({ limit, page, filters }));
-    } else if (fullPath === '/best-sellers') {
-      dispatch(fetchBestSellers({ limit, page, filters }));
-    } else if (fullPath === '/flash-sales') {
-      dispatch(fetchFlashSales({ limit, page, filters }));
-    } else {
-      dispatch(fetchProductsByCategories({ limit, page, filters, category: fullPath }));
+    if (prevPathname.current !== pathname) {
+      dispatch(resetFilters());
+      setIsFiltersLoaded(false);
+      prevPathname.current = pathname;
+
+      navigate({ search: '' }, { replace: true });
     }
-  }, [dispatch, limit, page, filters, fullPath, pathname]);
+  }, [pathname, dispatch, navigate]);
+
+  useEffect(() => {
+    if (!isFiltersLoaded) {
+      const params = queryString.parse(search, { arrayFormat: 'bracket' });
+      if (params.minPrice && params.maxPrice) {
+        dispatch(setPriceRange([Number(params.minPrice), Number(params.maxPrice)]));
+      }
+
+      if (params.sortBy) {
+        dispatch(setSortBy(params.sortBy as SortBy));
+      }
+
+      Object.entries(params).forEach(([key, value]) => {
+        if (key !== 'minPrice' && key !== 'maxPrice' && key !== 'sortBy') {
+          dispatch(setSpecificFilter({ key, value }));
+        }
+      });
+
+      setIsFiltersLoaded(true);
+    }
+  }, [dispatch, search, isFiltersLoaded]);
+
+  useEffect(() => {
+    if (isFiltersLoaded) {
+      const query = {
+        minPrice: filters.priceRange[0],
+        maxPrice: filters.priceRange[1],
+        sortBy: filters.sortBy,
+        ...filters.specificFilters,
+      };
+
+      const newSearch = queryString.stringify(query, { arrayFormat: 'bracket' });
+      navigate({ search: newSearch }, { replace: true });
+    }
+  }, [filters, navigate, isFiltersLoaded]);
+
+  useEffect(() => {
+    if (isFiltersLoaded) {
+      dispatch(clearProducts());
+      if (pathname === '/products') {
+        dispatch(fetchProducts({ limit, page, filters }));
+      } else if (fullPath === '/best-sellers') {
+        dispatch(fetchBestSellers({ limit, page, filters }));
+      } else if (fullPath === '/flash-sales') {
+        dispatch(fetchFlashSales({ limit, page, filters }));
+      } else {
+        dispatch(fetchProductsByCategories({ limit, page, filters, category: fullPath }));
+      }
+    }
+  }, [dispatch, limit, page, filters, fullPath, pathname, isFiltersLoaded]);
 
   return (
     <ProductsPage
