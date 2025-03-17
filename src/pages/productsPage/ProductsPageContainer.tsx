81531f8e6be113ec864bf0dd3d9f8b9687c1eef3
Andrey <andreybespaliy69@gmail.com>
2025-03-17 11:40:04 +0200

    feat: save applied filters in URL query
    - add ability to persist selected filters in the URL
    - use 'query-string' library for parsing and serializing query parameters

--- src/pages/productsPage/ProductsPageContainer.tsx
+++ src/pages/productsPage/ProductsPageContainer.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
 
 import { useAppDispatch, useAppSelector } from '@/app/hooks';
 import {
@@ -14,7 +15,11 @@ import {
   fetchFlashSales,
   selectFlashSales,
 } from '@features/products/productsSlice';
-import { selectAllFilters } from '@features/productFilters/productFiltersSlice';
+import {
+  selectAllFilters,
+  setPriceRange,
+  setSpecificFilter,
+} from '@features/productFilters/productFiltersSlice';
 import { Utils } from '@utils/index';
 
 import ProductsPage from './ProductsPage';
@@ -32,24 +37,55 @@ const ProductPageContainer = () => {
   const [limit, setLimit] = useState(20);
   const [page, setPage] = useState(1);
 
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
   const fullPath = pathname.replace('/products', '');
+  const navigate = useNavigate();
+  const [isFiltersLoaded, setIsFiltersLoaded] = useState(false);
 
   const categoryName = Utils.getCategoryName(fullPath);
   const breadCrumbs = Utils.generateBreadcrumbs(pathname);
 
   useEffect(() => {
-    dispatch(clearProducts());
-    if (pathname === '/products') {
-      dispatch(fetchProducts({ limit, page, filters }));
-    } else if (fullPath === '/best-sellers') {
-      dispatch(fetchBestSellers({ limit, page, filters }));
-    } else if (fullPath === '/flash-sales') {
-      dispatch(fetchFlashSales({ limit, page, filters }));
-    } else {
-      dispatch(fetchProductsByCategories({ limit, page, filters, category: fullPath }));
+    const params = queryString.parse(search, { arrayFormat: 'bracket' });
+    if (params.minPrice && params.maxPrice) {
+      dispatch(setPriceRange([Number(params.minPrice), Number(params.maxPrice)]));
     }
-  }, [dispatch, limit, page, filters, fullPath, pathname]);
+
+    Object.entries(params).forEach(([key, value]) => {
+      if (key !== 'minPrice' && key !== 'maxPrice') {
+        dispatch(setSpecificFilter({ key, value }));
+      }
+    });
+
+    setIsFiltersLoaded(true);
+  }, [dispatch, search]);
+
+  useEffect(() => {
+    const query = {
+      minPrice: filters.priceRange[0],
+      maxPrice: filters.priceRange[1],
+      ...filters.specificFilters,
+    };
+
+    const newSearch = queryString.stringify(query, { arrayFormat: 'bracket' });
+
+    navigate({ search: newSearch }, { replace: true });
+  }, [filters, navigate]);
+
+  useEffect(() => {
+    if (isFiltersLoaded) {
+      dispatch(clearProducts());
+      if (pathname === '/products') {
+        dispatch(fetchProducts({ limit, page, filters }));
+      } else if (fullPath === '/best-sellers') {
+        dispatch(fetchBestSellers({ limit, page, filters }));
+      } else if (fullPath === '/flash-sales') {
+        dispatch(fetchFlashSales({ limit, page, filters }));
+      } else {
+        dispatch(fetchProductsByCategories({ limit, page, filters, category: fullPath }));
+      }
+    }
+  }, [dispatch, limit, page, filters, fullPath, pathname, isFiltersLoaded]);
 
   return (
     <ProductsPage
